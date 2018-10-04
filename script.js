536670ff39fc8d669403fdcbf479567bd099b8d9
kirannoor <38943182+kirannoor@users.noreply.github.com>
2018-10-04 17:57:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Write all your Javascript here!
+function initialize(){
+{
+    $.ajax({
+	url: "https://techkaro-test.herokuapp.com/api/v1/getdata",
+	success: function(data){
+		let bodyEl= document.querySelector(".flower-div");
+		bodyEl.innerHTML=` 
+	
+
+})
+}
